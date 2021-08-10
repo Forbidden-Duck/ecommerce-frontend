cf535522c58bdbbde884de727a063f7222462208
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-10 14:07:44 +1000

    dotenv for holding the BASEURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "/",
-    withCredentials: true
+    baseURL: process.env.BASEURL || "http://localhost:6001/",
+    withCredentials: true,
 });
--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,9 @@ import App from './App';
 import rootReducer from './store/rootReducer'; 
 import './index.css';
 
+// Load environment variables
+require("dotenv").config({ path: __dirname + "/../process.env" });
+
 // Initialise redux store
 const store = configureStore({reducer: rootReducer});
 
