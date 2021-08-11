229e9002da9c5994dcad9cc36edc2cd42aab0412
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-11 16:49:18 +1000

    Attempt to refresh the token if the user isn't logged in (resets every refresh)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     BrowserRouter as Router,
     Redirect,
@@ -6,6 +6,9 @@ import {
     Switch,
 } from "react-router-dom";
 import "./App.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { refreshUserToken } from "./store/auth/Auth.actions";
 
 // TODO Routes
 import Home from "./routes/Home/Home";
@@ -17,6 +20,32 @@ import Navbar from "./components/Navbar/Navbar";
 // TODO AdminRoute (For admin specific routes)
 
 function App() {
+    const dispatch = useDispatch();
+    const { isAuthenticated, jwt, refreshError, isPending } = useSelector(
+        (state) => state.auth
+    );
+
+    if (!isAuthenticated && !refreshError && !isPending) {
+        dispatch(refreshUserToken());
+    }
+
+    // const [timer, setTimer] = useState(null);
+
+    // console.log(timer);
+    // if (isAuthenticated && !timer) {
+    //     const expiryDate = new Date(jwt.expiresIn);
+    //     const difference = expiryDate.getTime() - new Date().getTime();
+    //     console.log("inside");
+    //     setTimer(
+    //         setTimeout(() => {
+    //             console.log("timeout");
+    //         }, 5000)
+    //     );
+    // } else if (!isAuthenticated && timer) {
+    //     clearTimeout(timer);
+    //     setTimer(null);
+    // }
+
     return (
         <div style={{ flex: 1, background: "#222" }}>
             <Router>
