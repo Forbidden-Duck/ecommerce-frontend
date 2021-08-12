c7c478fc7765359e261083b1f636bdcfbd63752f
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-12 18:18:37 +1000

    Split section and home styles to a .css (support a variety of screen sizes)

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import "./Home.css";
 
 function Home() {
     const { authenticatedUser } = useSelector((state) => state.user);
@@ -9,38 +10,24 @@ function Home() {
     const glowColour = darkMode ? Array(3).fill("255") : Array(3).fill("0");
 
     const useStyles = makeStyles((theme) => ({
-        section: {
-            position: "relative",
-            width: "100%",
-            height: "100%",
-            textAlign: "center",
-            color: "#d7dee0",
-        },
         background: {
             backgroundImage: "url(/images/sunsetBackground.jpg)",
             backgroundRepeat: "no-repeat",
-            backgroundPosition: "left",
+            backgroundPosition: "center",
             backgroundSize: "cover",
             position: "absolute",
             top: "20%",
-            left: "-0.05%",
             width: "100%",
             height: "50%",
             boxShadow: `0px 1px 3px rgba(0, 0, 0, 0.05) inset, 0px 0px 20px rgba(${glowColour}, 1)`,
-        },
-        text: {
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -80%)",
         },
     }));
     const classes = useStyles();
 
     return (
-        <section className={classes.section}>
+        <section className="home-section">
             <div className={classes.background} />
-            <Typography className={classes.text} variant="h1">
+            <Typography className="home-text" variant="h1">
                 Welcome
                 {authenticatedUser &&
                     authenticatedUser.firstname &&
