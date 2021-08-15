01db2009e985a8897b4f9c3788cbccd77fe52d75
Harrison Howard <harrison.howard00707@gmail.com>
2021-08-15 17:47:14 +1000

    LoggedInRoute should wait for authentication to be finish before deciding to redirect you

--- src/components/LoggedInRoute/LoggedInRoute.jsx
+++ src/components/LoggedInRoute/LoggedInRoute.jsx
@@ -3,12 +3,12 @@ import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function LoggedInRoute({ Component, ...rest }) {
-    const { isAuthenticated } = useSelector((state) => state.auth);
+    const { isAuthenticated, isPending } = useSelector((state) => state.auth);
     return (
         <Route
             {...rest}
             render={(props) =>
-                isAuthenticated ? (
+                isAuthenticated || isPending ? (
                     <Component {...props} />
                 ) : (
                     <Redirect to="/login" />
