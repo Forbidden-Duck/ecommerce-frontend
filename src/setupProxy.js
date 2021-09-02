50e23d01360abcda8552351ecdb1b1b37dc5a875
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-03 00:33:41 +1000

    Log BASEURL variable for deployment testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
+    console.log(process.env.REACT_APP_BASEURL);
     app.use(
         createProxyMiddleware(["/api", "/auth"], {
             target: process.env.REACT_APP_BASEURL || "http://localhost:6001/",
