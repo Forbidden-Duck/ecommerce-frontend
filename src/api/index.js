9ebf03a8dcc783fe1e53aff73957e33ce5389633
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-03 00:20:54 +1000

    setupProxy

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BASEURL || "http://localhost:6001/",
+    baseURL: "/",
 });
