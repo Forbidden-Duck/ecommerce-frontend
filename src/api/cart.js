3e5f7aab45b209d0e91d3b6a58b838094df81b4f
Harrison Howard <harrison.howard00707@gmail.com>
2021-09-04 17:10:04 +1000

    Cart API and actions

---
+++ src/api/cart.js
@@ -0,0 +1,97 @@
+import API from "./";
+
+export const findMany = async (filter, token) => {
+    try {
+        return (
+            await API.get("api/cart", {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const getOne = async (cartid, token) => {
+    try {
+        return (
+            await API.get(`api/cart/${cartid}`, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const createOne = async (token, data) => {
+    try {
+        return (
+            await API.post("api/cart", data, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const deleteOne = async (cartid, token) => {
+    try {
+        return (
+            await API.delete(`api/cart/${cartid}`, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const addItemOne = async (cartid, token, data) => {
+    try {
+        return (
+            await API.post(`api/cart/${cartid}/items`, data, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const updateItemOne = async (cartid, cartitemid, token, data) => {
+    try {
+        return (
+            await API.put(`api/cart/${cartid}/items/${cartitemid}`, data, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const deleteItemOne = async (cartid, cartitemid, token) => {
+    try {
+        return (
+            await API.delete(`api/cart/${cartid}/items/${cartitemid}`, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
+
+export const checkout = async (cartid, token) => {
+    try {
+        return (
+            await API.post(`api/cart/${cartid}`, undefined, {
+                headers: { authorization: `Bearer ${token}` },
+            })
+        ).data;
+    } catch (err) {
+        throw err.response.data;
+    }
+};
---
+++ src/store/cart/Cart.actions.js
@@ -0,0 +1,95 @@
+import { createAsyncThunk, createAction } from "@reduxjs/toolkit";
+import * as cartAPI from "../../api/cart";
+
+export const findCarts = createAsyncThunk(
+    "api/findCarts",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.findMany(data.filter, data.token);
+        return {
+            carts: res,
+        };
+    }
+);
+
+export const getCart = createAsyncThunk(
+    "api/getCart",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.getOne(data.cartid, data.token);
+        return {
+            cart: res,
+        };
+    }
+);
+
+export const createCart = createAsyncThunk(
+    "api/createCart",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.createOne(data.token, {
+            adminBody: data.adminBody,
+        });
+        return {
+            cart: res,
+        };
+    }
+);
+
+export const addCartItem = createAsyncThunk(
+    "api/addCartItem",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.addItemOne(
+            data.cartid,
+            data.token,
+            data.cartitem
+        );
+        return {
+            cart: res.cart,
+            cartitem: res.cartitem,
+        };
+    }
+);
+
+export const updateCartItem = createAsyncThunk(
+    "api/updateCartItem",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.updateItemOne(
+            data.cartid,
+            data.cartitemid,
+            data.token,
+            data.cartitem
+        );
+        return {
+            cart: res.cart,
+            cartitem: res.cartitem,
+        };
+    }
+);
+
+export const deleteCartItem = createAsyncThunk(
+    "api/deleteCartItem",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.deleteItemOne(
+            data.cartid,
+            data.cartitemid,
+            data.token
+        );
+        return {
+            cartid: data.cartid,
+            cartitemid: data.cartitemid,
+        };
+    }
+);
+
+export const checkoutCart = createAsyncThunk(
+    "api/checkoutCart",
+    async (data, thunkAPI) => {
+        const res = await cartAPI.checkout(data.cartid, data.token);
+        return {
+            order: res.order,
+            charge: res.charge,
+        };
+    }
+);
+
+export const clearCartError = createAction("api/clearCartError");
+export const getCartFromCache = createAction("api/getCartFromCache");
+export const deleteCartFromCache = createAction("api/deleteCartFromCache");
